17f7f0567d866a95fbf0bcc2a82ce8ed9f9ceb3a
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2019-03-13 17:25:55 +0200

    added supporting multiline titles

--- Classes/MenuItemView.swift
+++ Classes/MenuItemView.swift
@@ -16,6 +16,7 @@ class MenuItemView: UIView {
     
     func setUpMenuItemView(_ menuItemWidth: CGFloat, menuScrollViewHeight: CGFloat, indicatorHeight: CGFloat, separatorPercentageHeight: CGFloat, separatorWidth: CGFloat, separatorRoundEdges: Bool, menuItemSeparatorColor: UIColor) {
         titleLabel = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: menuItemWidth, height: menuScrollViewHeight - indicatorHeight))
+        titleLabel?.numberOfLines = 0
         
         menuItemSeparator = UIView(frame: CGRect(x: menuItemWidth - (separatorWidth / 2), y: floor(menuScrollViewHeight * ((1.0 - separatorPercentageHeight) / 2.0)), width: separatorWidth, height: floor(menuScrollViewHeight * separatorPercentageHeight)))
         menuItemSeparator!.backgroundColor = menuItemSeparatorColor
@@ -28,14 +29,6 @@ class MenuItemView: UIView {
         self.addSubview(menuItemSeparator!)
         
         self.addSubview(titleLabel!)
-    }
-    
-    func setTitleText(_ text: NSString) {
-        if titleLabel != nil {
-            titleLabel!.text = text as String
-            titleLabel!.numberOfLines = 0
-            titleLabel!.sizeToFit()
-        }
     }
     
     func configure(for pageMenu: CAPSPageMenu, controller: UIViewController, index: CGFloat) {
